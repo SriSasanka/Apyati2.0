0097c334d7c7705383a5bf5975126895c49eed3b
SriSasanka <srisasanka.is@gmail.com>
2018-08-19 20:27:44 +0530

    Committed on 19-08-2018 at 20:08 PM by SriSasanka.

---
+++ src/app/pages/referral-details/referral-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-referral-details',
+  templateUrl: './referral-details.component.html',
+  styleUrls: ['./referral-details.component.css']
+})
+export class ReferralDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
